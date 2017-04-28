c4f4fb9bd877b2a6c6202468f1e67d8e93f9fcd3
sandygithub17 <santhoshkumar.scse@ksrce.ac.in>
2017-04-28 10:31:39 +0530

    Add files via upload

---
+++ PowerCal.java
@@ -0,0 +1,17 @@
+package guvi;
+
+import java.util.Scanner;
+import java.lang.*;
+
+public class PowerCal {
+	public static void main(String[] args) {
+		Scanner ss=new Scanner(System.in);
+		System.out.println("Enter the base number");
+		double a=ss.nextDouble();
+		System.out.println("Enter the exponent number");
+		double b=ss.nextDouble();
+		System.out.println("Power of number"+Math.pow(a, b));
+
+	}
+
+}
