a328743ce4ff63c717674c85d34c4df6bb6a0036
sandygithub17 <santhoshkumar.scse@ksrce.ac.in>
2017-06-06 12:15:45 +0530

    Add files via upload

--- Factorial.java
+++ Factorial.java
@@ -1,19 +1,20 @@
-package guvi;
+package santhosh;
 
 import java.util.Scanner;
 
 public class Factorial {
 
 	public static void main(String[] args) {
-		Scanner ss=new Scanner(System.in);
-		System.out.println("Enter the number:");
-		int num=ss.nextInt();
+		Scanner ss = new Scanner(System.in);
+		System.out.println("Enter the number");
+		int num = ss.nextInt();
 		int fact=1;
+		//int result=0;
 		for(int i=1;i<=num;i++) {
 			fact*=i;
 		}
-		System.out.println(num+" factorial is "+fact);
-
+		System.out.println(fact);
+		ss.close();
 	}
 
 }
