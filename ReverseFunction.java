8ef3c9f1ba4a576b34483694cb7a34b5965498d9
sandygithub17 <santhoshkumar.scse@ksrce.ac.in>
2017-06-20 10:18:40 +0530

    Add files via upload

---
+++ ReverseFunction.java
@@ -0,0 +1,26 @@
+package guvi;
+
+import java.util.Scanner;
+
+class Formatter { 
+	 public static String reverseWord(String str){  
+   String words[]=str.split("\\s");  
+   String reverseWord="";  
+   for(String w:words){  
+       StringBuilder sb=new StringBuilder(w);  
+       sb.reverse();  
+       reverseWord+=sb.toString()+" ";  
+   }  
+   return reverseWord.trim();  
+}  
+}  
+public class ReverseFunction {
+
+	public static void main(String[] args) {
+		Scanner ss = new Scanner(System.in);
+		String str = ss.nextLine();
+		System.out.println(Formatter.reverseWord(str));
+		ss.close();
+	}
+
+}
