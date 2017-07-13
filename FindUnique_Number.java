5e31e2d1532d15ff11230cd4ba848830e31dd91f
sandygithub17 <santhoshkumar.scse@ksrce.ac.in>
2017-07-13 15:18:20 +0530

    Add files via upload
    find unique elements in an given array

---
+++ FindUnique_Number.java
@@ -0,0 +1,21 @@
+package guvi;
+
+public class FindUnique_Number {
+
+	public static void main(String[] args) {
+		
+		int[] arr = {1,2,6,9,9,1,3,6,2};
+		for(int i = 0; i < arr.length; i++) {
+			int count = 0;
+			for(int j = 0; j < arr.length; j++) {
+				if(arr[i] == arr[j]) {
+					count++;
+				}
+			}
+			if(count < 2) {
+				System.out.print(arr[i]+" ");
+			}
+		}
+	}
+
+}
