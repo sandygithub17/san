cd4737c34818573a8ea535352490d795d507c483
sandygithub17 <santhoshkumar.scse@ksrce.ac.in>
2017-07-14 12:00:20 +0530

    Add files via upload
    Find the tallest man in the given class room and 
    also find 4th tallest student and given Kth tallest student

---
+++ FindTallestStudent.java
@@ -0,0 +1,22 @@
+package guvi;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class FindTallestStudent {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> al = new ArrayList<Integer>();
+		Scanner ss = new Scanner(System.in);
+		for(int i = 0; i < 10; i++) {
+			al.add(ss.nextInt());//Enter the height in Centimeter
+		}
+		Collections.sort(al);
+		System.out.println();
+		System.out.println("4th tallest Student in the class "+al.get(3));
+		int k = ss.nextInt();//Enter the k value for finding a particular value
+		System.out.println("Kth tallest student in the class "+al.get(k-1));
+
+	}
+}
