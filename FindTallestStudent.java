090d8c4c2cca0cc82dee3a510d821d399423946a
sandygithub17 <santhoshkumar.scse@ksrce.ac.in>
2017-07-14 12:01:23 +0530

    Update FindTallestStudent.java

--- FindTallestStudent.java
+++ FindTallestStudent.java
@@ -9,7 +9,7 @@ public class FindTallestStudent {
 	public static void main(String[] args) {
 		ArrayList<Integer> al = new ArrayList<Integer>();
 		Scanner ss = new Scanner(System.in);
-		for(int i = 0; i < 10; i++) {
+		for(int i = 0; i < 50; i++) {
 			al.add(ss.nextInt());//Enter the height in Centimeter
 		}
 		Collections.sort(al);
