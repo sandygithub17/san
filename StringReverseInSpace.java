11c222af9ea502ce68c2f26e216585afedca983a
sandygithub17 <santhoshkumar.scse@ksrce.ac.in>
2017-08-05 11:19:35 +0530

    Add files via upload

---
+++ StringReverseInSpace.java
@@ -0,0 +1,23 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class StringReverseInSpace {
+	public static void main(String[] args) {
+		Scanner ss = new Scanner(System.in);
+		System.out.println("Enter the string");
+		String str = ss.nextLine();
+		String[] splitStr = str.split("\\ ");
+		for(int i = 0; i < splitStr.length; i++) {
+			String rev = new StringBuffer(splitStr[i]).reverse().toString();
+			String first = rev.substring(0, 1).toUpperCase();
+			String last = rev.substring(rev.length() - 1).toLowerCase();
+			String second = rev.substring(1, rev.length()-1);
+			System.out.print(first+""+second+""+last);
+			System.out.print(" ");
+		}
+		ss.close();
+	}
+
+
+}
